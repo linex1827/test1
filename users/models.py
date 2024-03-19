b02be2cc214ba56a8c376b3cee8372833781859d
linex1827 <103201373+linex1827@users.noreply.github.com>
2024-03-19 12:19:30 +0600

    new_merge

--- users/models.py
+++ users/models.py
@@ -4,3 +4,7 @@ from django.db import models
 # Create your models here.
 class CustomUser(models.Model):
     cd = models.CharField(max_length=200)
+    cddd = models.CharField(max_length=250)
+    new = models.CharField(max_length=150)
+    new = models.IntegerField()
+    merge_new = models.CharField(max_length=150)
