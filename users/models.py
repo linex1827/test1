6f4b2e65daaefe0aab628395f02dc04a0ad74c58
linex1827 <103201373+linex1827@users.noreply.github.com>
2024-03-19 12:39:33 +0600

    delete2

--- users/models.py
+++ users/models.py
@@ -7,6 +7,5 @@ class CustomUser(models.Model):
     cddd = models.CharField(max_length=250)
     new = models.CharField(max_length=150)
     new = models.IntegerField()
-    merge_new = models.CharField(max_length=150)
 
 
